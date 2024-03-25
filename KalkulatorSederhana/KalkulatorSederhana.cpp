02ebeee05f89c269e7121eaae2de635d05f62366
M S I <M S I@MSI>
2024-03-25 10:18:38 +0700

    input menu

--- KalkulatorSederhana/KalkulatorSederhana.cpp
+++ KalkulatorSederhana/KalkulatorSederhana.cpp
@@ -25,5 +25,24 @@ int perkalian(int bil_1, int bil_2) {
 }
 
 int pembagian(int bil_1, int bil_2) {
-	return bil_1 : bil_2;
+	return bil_1 / bil_2;
+}
+
+int main()
+{
+	int pilihan;
+
+	do {
+		system("cls");
+		cout << "==== Menu =====" << endl;
+		cout << "1. Penjumlahan" << endl;
+		cout << "2. Pengurangan" << endl;
+		cout << "3. Perkalian" << endl;
+		cout << "4. Pembagian" << endl;
+		cout << "5. Keluar" << endl;
+		cout << "Masukkan menu yang ingin di pilih (1/2/3/4/5): ";
+		cin >> pilihan;
+
+		
+	}
 }
